2e7bb575f593bd55d9493724722e57c768e07a30
Davi Holanda <davi.holanda90@gmail.com>
2021-03-02 17:02:25 -0300

    Tirei as id's das abas como constantes, ficaram como parâmetros a serem obtidas como variáveis de ambiente

--- user_adder.py
+++ user_adder.py
@@ -1,7 +1,7 @@
 from google_sheets_utilities import*
 from google_sheets_constants import*
 
-def add_user(sheet, spread_sheet_id, user_name):
+def add_user(sheet, spread_sheet_id, tab_id, user_name):
     ''' Pega a ID da planilha e o nome do novo usuário
         Adiciona usuário no início da aba de Upsolving ou avisa que já tem um usuário com esse nome '''
     user_exist = get_user_column_id(sheet, spread_sheet_id, user_name)
@@ -22,19 +22,19 @@ def add_user(sheet, spread_sheet_id, user_name):
                 users_quantity = int(j)
         
         # Inserindo coluna no início
-        insert_columns(sheet, spread_sheet_id, UPSOLVING_TAB_ID, USERS_START_COLUMN_ID, 1)
+        insert_columns(sheet, spread_sheet_id, tab_id, USERS_START_COLUMN_ID, 1)
 
         # Formatando a coluna caso seja o primeiro usuário a ser adicionado
         if users_quantity == 0:
             format_problem_cells(
-                sheet, spread_sheet_id, UPSOLVING_TAB_ID,
+                sheet, spread_sheet_id, tab_id,
                 PROBLEMS_START_ROW, USERS_START_COLUMN_ID,
                 problems_end_row, USERS_START_COLUMN_ID
             )
-            change_column_size(sheet, spread_sheet_id, UPSOLVING_TAB_ID, USERS_START_COLUMN_ID, 1, USERS_COLUMN_SIZE)
-            format_user_cells(sheet, spread_sheet_id, UPSOLVING_TAB_ID, USERS_ROW, USERS_START_COLUMN_ID)
-            centralize_cells(sheet, spread_sheet_id, UPSOLVING_TAB_ID, USERS_ROW + 1, USERS_START_COLUMN_ID, problems_end_row, USERS_START_COLUMN_ID)
-            bold_cells(sheet, spread_sheet_id, UPSOLVING_TAB_ID, USERS_ROW, USERS_START_COLUMN_ID, PROBLEM_COUNTER_ROW, USERS_START_COLUMN_ID)
+            change_column_size(sheet, spread_sheet_id, tab_id, USERS_START_COLUMN_ID, 1, USERS_COLUMN_SIZE)
+            format_user_cells(sheet, spread_sheet_id, tab_id, USERS_ROW, USERS_START_COLUMN_ID)
+            centralize_cells(sheet, spread_sheet_id, tab_id, USERS_ROW + 1, USERS_START_COLUMN_ID, problems_end_row, USERS_START_COLUMN_ID)
+            bold_cells(sheet, spread_sheet_id, tab_id, USERS_ROW, USERS_START_COLUMN_ID, PROBLEM_COUNTER_ROW, USERS_START_COLUMN_ID)
 
             # Colocando as bordas
             range = (
@@ -48,12 +48,12 @@ def add_user(sheet, spread_sheet_id, user_name):
             requests = []
             for i in cells:
                 for j in i:
-                    requests.append(external_border_request(UPSOLVING_TAB_ID, start_row, USERS_START_COLUMN_ID, end_row, USERS_START_COLUMN_ID))
+                    requests.append(external_border_request(tab_id, start_row, USERS_START_COLUMN_ID, end_row, USERS_START_COLUMN_ID))
                     start_row = end_row + 1
                 end_row = end_row + 1
             if problems_end_row >= PROBLEMS_START_ROW:
-                requests.append(external_border_request(UPSOLVING_TAB_ID, start_row, USERS_START_COLUMN_ID, problems_end_row, USERS_START_COLUMN_ID))
-            requests.append(external_border_request(UPSOLVING_TAB_ID, USERS_ROW, USERS_START_COLUMN_ID, USERS_ROW, USERS_START_COLUMN_ID))
+                requests.append(external_border_request(tab_id, start_row, USERS_START_COLUMN_ID, problems_end_row, USERS_START_COLUMN_ID))
+            requests.append(external_border_request(tab_id, USERS_ROW, USERS_START_COLUMN_ID, USERS_ROW, USERS_START_COLUMN_ID))
             body = {"requests": requests}
             sheet.batchUpdate(spreadsheetId=spread_sheet_id, body=body).execute()
 
@@ -80,7 +80,7 @@ def add_user(sheet, spread_sheet_id, user_name):
         row = PROBLEMS_START_ROW
         requests = []
         while row <= problems_end_row:
-            requests.append(write_request(UPSOLVING_TAB_ID,row,USERS_START_COLUMN_ID,"N"))
+            requests.append(write_request(tab_id,row,USERS_START_COLUMN_ID,"N"))
             row = row + 1
         body = {"requests": requests}
         sheet.batchUpdate(spreadsheetId=spread_sheet_id, body=body).execute()
@@ -94,7 +94,7 @@ def add_user(sheet, spread_sheet_id, user_name):
             body = {"values":[[new_users_quantity]]}
         ).execute()
 
-def delete_user(sheet, spread_sheet_id, user_name):
+def delete_user(sheet, spread_sheet_id, tab_id, user_name):
     ''' Pega a ID da planilha e o nome do usuário que quer deletar
         Deleta a coluna do Usuário ou avisa que o usuário não foi encontrado '''
     #deletando a coluna do usuário
@@ -102,7 +102,7 @@ def delete_user(sheet, spread_sheet_id, user_name):
     if(user_column_id == None):
         print("Usuário não encontrado")
     else:
-        delete_columns(sheet, spread_sheet_id, UPSOLVING_TAB_ID, user_column_id, 1)
+        delete_columns(sheet, spread_sheet_id, tab_id, user_column_id, 1)
         
         # Atualizando a quantidade de usuários
         range = (
